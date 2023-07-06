88f8458911a1c6ec047ff0b196a56b6aca68bb4e
acharyamanish006 <manish.acharya313@gmail.com>
2023-07-06 21:47:36 +0545

    mergeSort algorithm

--- mergeArray.js
+++ mergeArray.js
@@ -1,28 +1,31 @@
-const notGood_mergeArray = (arr1, arr2) => {
-  let newArr = [];
-  let loopLength;
-  if (arr1.length > arr2.length) {
-    loopLength = arr1.length;
-  } else {
-    loopLength = arr2.length;
-  }
+// const notGood_mergeArray = (arr1, arr2) => {
+//   let newArr = [];
+//   let loopLength;
+//   if (arr1.length > arr2.length) {
+//     loopLength = arr1.length;
+//   } else {
+//     loopLength = arr2.length;
+//   }
 
-  for (let i = 0; i < loopLength; i++) {
-    if (arr1[i] < arr2[i]) {
-      newArr.push(arr1[i]);
-      newArr.push(arr2[i]);
-    } else {
-      newArr.push(arr2[i]);
-      newArr.push(arr1[i]);
-    }
-  }
-  console.log(newArr);
-};
+//   for (let i = 0; i < loopLength; i++) {
+//     if (arr1[i] < arr2[i]) {
+//       newArr.push(arr1[i]);
+//       newArr.push(arr2[i]);
+//     } else {
+//       newArr.push(arr2[i]);
+//       newArr.push(arr1[i]);
+//     }
+//   }
+//   console.log(newArr);
+// };
 
 const mergeArray = (arr1, arr2) => {
   let i = 0;
   let j = 0;
   let newArr = [];
+  if (arr1 == undefined) {
+    return;
+  }
 
   while (i < arr1.length && j < arr2.length) {
     if (arr1[i] < arr2[j]) {
@@ -42,6 +45,15 @@ const mergeArray = (arr1, arr2) => {
     j++;
   }
   console.log(newArr);
+  return newArr;
 };
 
-mergeArray([1, 4, 6, 17, 20], [2, 3, 5, 8, 9]);
+const mergeSort = (arr) => {
+  if (arr.length <= 1) return arr;
+
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return mergeArray(left, right);
+};
+mergeSort([0, 6, 8, 91, 55, 77, 3, 9]);
