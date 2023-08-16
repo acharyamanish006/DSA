04e286ee318ea3e3511d2c1468516aa2bc2805b5
acharyamanish006 <manish.acharya313@gmail.com>
2023-08-16 19:39:05 +1000

    singlyLinkedList some exe

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -7,9 +7,29 @@ class Node {
 
 class SinglyLinkedList {
   constructor() {
+    this.head = null;
+    this.tail = null;
     this.length = 0;
   }
   push(val) {
     let node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = this.head;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length += 1;
+    return this;
   }
 }
+
+let newList = new SinglyLinkedList();
+newList.push("hey");
+newList.push("hi");
+newList.push("how");
+newList.push("are");
+newList.push("you");
+
+console.log(newList);
