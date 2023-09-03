c3b5153d0a898d7e7bacec21fc9e7a1faaf9544b
acharyamanish006 <manish.acharya313@gmail.com>
2023-09-03 13:38:26 +1000

    maxProfit leetcode

--- maxProfit.js
+++ maxProfit.js
@@ -1,12 +1,24 @@
 var maxProfit = function (prices) {
-  let buy;
-  let sell;
-  for (let i = 0; i < prices.length; i++) {
-    for (let j = i + 1; j < prices.length; j++) {
-      if (prices[j] > prices[i]) {
-        console.log("sell");
-      }
+  let l = 0;
+  let r = 1;
+  let maxProfit = 0;
+  while (r < prices.length) {
+    let profit = 0;
+    if (prices[l] > prices[r]) {
+      console.log(prices[l]);
+      l = r;
+      console.log(prices[l]);
     }
+    profit = prices[r] - prices[l];
+    console.log(`P${profit}`);
+    if (maxProfit < profit) {
+      console.log(`p${profit}`);
+      maxProfit = profit;
+    }
+    r++;
   }
+  console.log(maxProfit);
+  return maxProfit;
 };
+
 maxProfit([7, 1, 5, 3, 6, 4]);
